7ed6e39c3e004f2760ef874bc624827e86c23088
Allan Lima <allan@mamangava.com>
2016-09-11 23:21:15 -0300

    Single Publicações - Artigo

--- wp-content/themes/maneira-advogados/template-parts/content-artigos.php
+++ wp-content/themes/maneira-advogados/template-parts/content-artigos.php
@@ -25,42 +25,9 @@
 	while ( $query->have_posts() ) : $query->the_post(); ?>
 		<div class="content-text-publicacoes-item">
 			<span class="publicacoes-date"><?php the_date(); ?></span>
-			<h3 class="publicacoes-title"><?php the_title(); ?></h3>
+			<a href="<?php the_permalink(); ?>" rel="bookmark"><h3 class="publicacoes-title"><?php the_title(); ?></h3></a>
 			<p class="publicacoes-content"><?php the_content(); ?></p>
 			<span class="text-uppercase publicacoes-author"><?php echo get_the_author(); ?></span>
 		</div>
 	<?php endwhile;
 ?>
-
-
-<!-- <div class="content-text-publicacoes-item">
-	<span class="publicacoes-date">07 de Junho, 2016</span>
-	<h3 class="publicacoes-title">Excelencia em advogacia tributária</h3>
-	<p class="publicacoes-content">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-	cillum dolore eu fugiat nulla pariatur.</p>
-	<span class="text-uppercase publicacoes-author">André Mendes Moreira</span>
-</div>
-<div class="content-text-publicacoes-item">
-	<span class="publicacoes-date">07 de Junho, 2016</span>
-	<h3 class="publicacoes-title">Excelencia em advogacia tributária</h3>
-	<p class="publicacoes-content">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-	cillum dolore eu fugiat nulla pariatur.</p>
-	<span class="text-uppercase publicacoes-author">André Mendes Moreira</span>
-</div>
-<div class="content-text-publicacoes-item">
-	<span class="publicacoes-date">07 de Junho, 2016</span>
-	<h3 class="publicacoes-title">Excelencia em advogacia tributária</h3>
-	<p class="publicacoes-content">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-	cillum dolore eu fugiat nulla pariatur.</p>
-	<span class="text-uppercase publicacoes-author">André Mendes Moreira</span>
-</div>
- -->
