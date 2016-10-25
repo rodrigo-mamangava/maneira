749c2b7e53dccc1aa78ec7d4e6528c13b7f5b389
Rodrigo <rodrigo@plau.co>
2016-10-25 11:43:09 -0200

    Ajustes Functions

--- wp-content/themes/maneira-advogados/functions.php
+++ wp-content/themes/maneira-advogados/functions.php
@@ -168,11 +168,3 @@ function add_search_to_wp_menu($items, $args) {
 }
 
 // add_filter('wp_nav_menu_items', 'add_search_to_wp_menu', 10, 2);
-
-function custom_active_item_classes($classes = array(), $menu_item = false){            
-        global $post;
-        $classes[] = ($menu_item->url == get_post_type_archive_link($post->post_type)) ? 'current-menu-item active' : '';
-        return $classes;
-    }
-add_filter( 'nav_menu_css_class', 'custom_active_item_classes', 10, 2 );
-
